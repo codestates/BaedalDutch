1f829cb4c9b040c84d1bca7bc99b9315665c8124
이성혁 <bladezerocd@gmail.com>
2022-04-20 17:15:21 +0900

    ADD:page 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,49 +1,49 @@
-import 'reflect-metadata';
-import { createConnection } from 'typeorm';
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import 'dotenv/config';
+import "reflect-metadata"
+import { createConnection } from "typeorm"
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import "dotenv/config"
 
 // modules
 // import boardRouter from './routers/boardRouter';
 
-const PORT = 80;
+const PORT = 4000
 
-const app = express();
-const logger = morgan('dev');
+const app = express()
+const logger = morgan("dev")
 
 // db connection
 createConnection()
   .then(() => {
-    console.log('Database Connected :)');
+    console.log("Database Connected :)")
     app.listen(PORT, () =>
-      console.log(`Server Listening on http://localhost:${PORT}`),
-    );
+      console.log(`Server Listening on http://localhost:${PORT}`)
+    )
   })
-  .catch(error => console.log(error));
+  .catch((error) => console.log(error))
 
 // middleware
-app.use(express.json()); //body parser(json)
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json()) //body parser(json)
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: false }))
 // app.use(express.static(__dirname+'/../src/imageStorage'));
-app.use(express.static('./src/imageStorage'));
+app.use(express.static("./src/imageStorage"))
 // app.use(express.static('.'));
 
-app.use(logger);
+app.use(logger)
 app.use(
   cors({
     origin: true,
     credentials: true,
-  }),
-);
+  })
+)
 
 // route
-app.get('/', (req, res) => {
-  res.send('server connected');
-});
+app.get("/", (req, res) => {
+  res.send("server connected")
+})
 
 // 라우터 연결
 // app.use('/', globalRouter);
