8b486735794dc0c95e50c043103206e2af610170
jungukuk <uk73020612@gmail.com>
2022-04-23 13:37:31 +0900

    deploy

---
+++ scripts/initialize.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/BaedalDutch/server
+npm install
+npm install pm2@latest -g
+sudo apt-get update
---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/BaedalDutch/server
+authbind --deep pm2 start index.js
