7d4cba844b8907cd8327b69329ffe67d14c1a970
xodud3008 <xodud3008@gmail.com>
2022-04-23 16:07:54 +0900

    baekcup

--- server/models/user.js
+++ server/models/user.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      models.user.belongsToMany(models.party, {
+        through: "user_party",
+        foreignKey: "user_id",
+      });
     }
   }
   user.init(
