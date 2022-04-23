84cd7b41f8aa10f4a2afa0d28547c39075a40a65
SeunghoLEE <suhole7728@naver.com>
2022-04-23 14:47:22 +0900

    ADD 데이터

---
+++ server/models/parties.js
@@ -0,0 +1,41 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class parties extends Model {
+    static associate(models) {
+      // models.parties.belongsTo(models.users, {
+      //   foreignKey: "writeUser_id",
+      //   targetKey: "id",
+      // });
+      // models.parties.hasMany(models.user_parties, {
+      //   foreignKey: "parties_id",
+      //   sourceKey: "id",
+      // });
+      models.parties.belongsToMany(models.users, {
+        through: "users_parties",
+        foreignKey: "parties_id",
+      });
+    }
+  }
+  parties.init(
+    {
+      writeUser_id: DataTypes.INTEGER,
+      store_name: DataTypes.STRING,
+      food_category: DataTypes.STRING,
+      member_num: DataTypes.INTEGER,
+      content: DataTypes.STRING,
+      fee: DataTypes.INTEGER,
+      address: DataTypes.STRING,
+      closed: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "parties",
+    }
+  );
+  return parties;
+};
