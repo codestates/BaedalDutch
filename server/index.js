74a6e0bc1d5f75882a46421736c29ac70b68ae4b
jungukuk <uk73020612@gmail.com>
2022-04-24 13:19:25 +0900

    test

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(
 );
 
 app.use(cookieParser());
-app.get("/", (req, res) => res.send("hellooooo world"));
+app.get("/", (req, res) => res.send("helloooooajashdfjkhjaskdhlksd world"));
 app.use("/users", usersRouter);
 app.use("/parties", partiesRouter);
 
