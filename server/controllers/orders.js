6642eb7b08291b52cef209b4043e3a490c49b8e3
jungukuk <uk73020612@gmail.com>
2022-04-28 09:14:12 +0900

    modified orders

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -4,14 +4,19 @@ const { isAuthorized } = require("./tokenfunctions");
 module.exports = {
   participateParty: async (req, res) => {
     // 파티에 참가
-    const userInfo = await isAuthorized(req);
-    if(!userInfo){
+    const userInfo = isAuthorized(req);
+    console.log("userinfo-------", userInfo);
+    if (!userInfo) {
       res.status(404).send({
-        "message": "Bad request update parties"
-     })
+        message: "Bad request update parties",
+      });
     }
-    parties.findOne({ where: { parties_id: req.params.id } });
-    res.end();
+    try {
+      users_parties.create({ users_id: userInfo.id, parties_id: req.params.id });
+      res.status(201).send({message: "success participate parties"})
+    } catch (err) {
+      res.end();
+    }
   },
   cancelOrder: async (req, res) => {
     // 파티에서 탈퇴
