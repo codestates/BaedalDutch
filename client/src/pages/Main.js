e0f8bc0fb72a2646ca74e83f0c1afd20ac9c730d
이성혁 <bladezerocd@gmail.com>
2022-04-28 10:19:19 +0900

    ADD:map 작성중

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,7 +1,30 @@
+import React, { useState } from 'react';
+import MapContainer from '../components/test';
+import styled from 'styled-components';
+
+const { kakao } = window;
+
 function Main() {
+  const [InputText, setInputText] = useState('');
+  const [Place, setPlace] = useState('');
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPlace(InputText);
+    setInputText('');
+  };
+
   return (
     <>
-      <div>Main</div>
+      <MapContainer searchPlace={Place} />
+      <form className="inputForm" onSubmit={handleSubmit}>
+        <input placeholder="검색어를 입력하세요" onChange={onChange} value={InputText} />
+        <button type="submit">검색</button>
+      </form>
     </>
   );
 }
