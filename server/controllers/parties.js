2369dfa9e4b2642907d8e3d974c2258562f57106
jungukuk <uk73020612@gmail.com>
2022-05-14 01:51:14 +0900

    test20

--- server/controllers/parties.js
+++ server/controllers/parties.js
@@ -187,7 +187,7 @@ module.exports = {
     // console.log('userInfo:', userInfo)
     // 2. 유저가 토큰을 가지고 있지 않는 경우
     if (!userInfo) {
-      res.status(404).json({
+      return res.status(404).json({
         message: 'token none',
       })
     }
@@ -216,17 +216,17 @@ module.exports = {
       )
       console.log('partyInfo', partyInfo)
       if (!partyInfo) {
-        res.status(404).json({
+        return res.status(404).json({
           message: 'Bad request update parties',
         })
       } else {
-        res.status(200).json({
+        return res.status(200).json({
           data: partyInfo,
           message: 'success update parties update parties',
         })
       }
     } catch (err) {
-      res.status(500).json({
+      return res.status(500).json({
         message: 'Server Error update parties',
       })
     }
