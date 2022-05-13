f75341e784a54c3ec571df93b09a5298351b630e
jungukuk <uk73020612@gmail.com>
2022-05-13 18:55:44 +0900

    test14

--- server/controllers/tokenfunctions/index.js
+++ server/controllers/tokenfunctions/index.js
@@ -7,9 +7,9 @@ module.exports = {
   },
   sendAccessToken: (res, accessToken) => {
     return res.status(200).cookie("jwt", accessToken, {
-      // httpOnly: true,
-      // secure: true,
-      // sameSite: "none",
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
       domain: ".baedaldutch-ukuk.s3-website-us-east-1.amazonaws.com",
     });
   },
